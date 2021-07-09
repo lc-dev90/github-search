2cbcd9a05b40a553215d555ad82d42b5f221a242
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-09 17:38:32 -0300

    Create routes, HomeScreen inital and ProfileScreen initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Components
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
 const App = () => {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/profile/:id" component={ProfileScreen} />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileScreen = () => {
+  return <div>Hello from profile page</div>;
+};
+
+export default ProfileScreen;

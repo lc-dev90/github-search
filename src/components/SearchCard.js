b0a57fd4deee6339a951530005d64776e7d96763
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-10 18:43:40 -0300

    added hover effects

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { User } from "@styled-icons/boxicons-regular/User";
 import { LinkExternal } from "@styled-icons/boxicons-regular/LinkExternal";
+import { Info } from "@styled-icons/evaicons-solid/Info";
 
 const SearchCard = ({ avatar, username, url }) => {
   return (
@@ -25,6 +26,12 @@ const SearchCard = ({ avatar, username, url }) => {
             {url}
           </a>
         </p>
+        <span className="information">
+          <i>
+            <InfoIcon />
+          </i>
+          <span>Click to view details</span>
+        </span>
       </div>
     </Card>
   );
@@ -34,11 +41,27 @@ export default SearchCard;
 
 const Card = styled.div`
   margin-bottom: 20px;
+  position: relative;
   display: flex;
   background: #201f1f;
   border-radius: 10px;
   padding: 30px;
   width: 600px;
+  cursor: pointer;
+  box-shadow: 0px 3px 17px -3px rgba(0, 0, 0, 0.49),
+    0px 3px 17px -2px rgba(0, 0, 0, 0.49);
+  &:hover {
+    box-shadow: 0px 1px 7px -1px rgba(0, 0, 0, 0.49),
+      0px 1px 7px -2px rgba(0, 0, 0, 0.49);
+    div {
+      &:nth-of-type(2) {
+        .information {
+          opacity: 1;
+          background: rgba(10, 10, 10, 0.79);
+        }
+      }
+    }
+  }
   div {
     &:nth-of-type(1) {
       width: 100px;
@@ -51,6 +74,30 @@ const Card = styled.div`
       display: flex;
       flex-direction: column;
       justify-content: space-evenly;
+      .information {
+        position: absolute;
+        height: 100%;
+        z-index: 1;
+        right: 0px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 10px;
+        opacity: 0;
+        transition: opacity ease-in-out 0.25s;
+        span {
+          font-size: 10px;
+          color: white;
+          margin-top: 8px;
+        }
+        i {
+          width: 30px;
+          display: inline-block;
+        }
+      }
       p {
         font-size: 12px;
         text-transform: uppercase;
@@ -78,5 +125,8 @@ const UserIcon = styled(User)`
   color: #8752cc;
 `;
 const LinkExternalIcon = styled(LinkExternal)`
+  color: #8752cc;
+`;
+const InfoIcon = styled(Info)`
   color: #8752cc;
 `;

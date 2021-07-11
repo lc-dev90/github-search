18a83bdf79f610a0b76406b6587dd16e4f07de52
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-10 22:19:44 -0300

    pagination

--- src/screens/SearchResultsScreen.js
+++ src/screens/SearchResultsScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Pagination } from "@material-ui/lab";
 import styled from "styled-components";
 import axios from "axios";
 
@@ -9,20 +10,41 @@ import Footer from "../components/Footer";
 
 const SearchResultsScreen = ({ location }) => {
   const [searchResults, setSearchResults] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [page, setPage] = useState(1);
+  const pages =
+    Number(Math.ceil(totalCount / 10)) > 100
+      ? 100
+      : Number(Math.ceil(totalCount / 10)) > 100;
+
+  const handleChangePagination = (e, v) => {
+    setPage(v);
+  };
 
   const query = location.search.split("=")[1];
 
-  /*  `https://api.github.com/search/users?q=${query}&page=2` */
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios.get(
-        `https://api.github.com/search/users?q=${query}`
+        `https://api.github.com/search/users?q=${query}&per_page=10`
       );
       setSearchResults(data.items);
+      setTotalCount(data.total_count);
     };
     fetchData();
-  }, []);
-  console.log(searchResults);
+  }, [query]);
+
+  useEffect(() => {
+    if (page) {
+      const fetchData = async () => {
+        const { data } = await axios.get(
+          `https://api.github.com/search/users?q=${query}&page=${page}&per_page=10`
+        );
+        setSearchResults(data.items);
+      };
+      fetchData();
+    }
+  }, [page, query]);
   return (
     <>
       <Header />
@@ -37,6 +59,17 @@ const SearchResultsScreen = ({ location }) => {
               />
             ))
           : "Loading..."}
+        {/* <div>
+          {Array.from(Array(pages).keys()).map((item) => (
+            <span style={{ marginRight: "4px" }}>{item + 1}</span>
+          ))}
+        </div> */}
+        <Pagination
+          style={{ marginTop: "20px" }}
+          count={pages}
+          className="pagination"
+          onChange={handleChangePagination}
+        />
       </SearchContainer>
       <Footer />
     </>
@@ -54,4 +87,16 @@ const SearchContainer = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  .pagination {
+    ul {
+      li {
+        div {
+          color: white;
+        }
+        button {
+          color: white;
+        }
+      }
+    }
+  }
 `;

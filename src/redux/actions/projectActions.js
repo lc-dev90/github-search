b8093e4d60fa3f9f7b5149877ec4c748ec3cf544
lc-dev90 <lc.danella.dev@gmail.com>
2021-07-13 22:48:12 -0300

    project details actions

--- src/redux/actions/projectActions.js
+++ src/redux/actions/projectActions.js
@@ -1,1 +1,30 @@
-/* `https://api.github.com/repos/${user}/${name}` */
+import axios from "axios";
+
+import {
+  PROJECT_DETAILS_FAIL,
+  PROJECT_DETAILS_SUCCESS,
+  PROJECT_DETAILS_REQUEST,
+} from "../constants/projectConstants";
+
+export const getProjectDetails = (user, project) => async (dispatch) => {
+  try {
+    dispatch({
+      PROJECT_DETAILS_REQUEST,
+    });
+    const projectDetails = axios.get(
+      `https://api.github.com/repos/${user}/${project}`
+    );
+    dispatch({
+      PROJECT_DETAILS_SUCCESS,
+      payload: projectDetails.data,
+    });
+  } catch (error) {
+    dispatch({
+      PROJECT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
